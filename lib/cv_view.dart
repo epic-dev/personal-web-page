d0e158e0c2b39807a6b28a27d2baae1ad1170919
Pavel Yuruts <PavelYuruts@coherentsolutions.com>
2022-11-18 01:02:33 +0300

    update

--- lib/cv_view.dart
+++ lib/cv_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class CVView extends StatelessWidget {
